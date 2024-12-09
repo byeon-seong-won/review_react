04ab1fa8326c32f96e0434559872f1a5c323378b
byeon-seong-won <sngoneee@gamil.com>
2024-12-09 22:26:43 +0900

    수정

---
+++ src/Alertdialog.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function Alertdialog() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+export default Alertdialog;
--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import Footerwrap from './Footer.js';
 import Card from './Card.js';
+import Alertdialog from './Alertdialog.js';
+import Button from './Button.js';
 import styled from 'styled-components';
 
 
